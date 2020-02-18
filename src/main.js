86a80f4411725384c0ebed8b6419825944a371ff
Eduardo Favela <jeffersongutierritos>
2020-02-18 16:07:48 -0600

    a

--- src/main.js
+++ src/main.js
@@ -58,6 +58,19 @@ app.use((req, res, next) => {
 // Public
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.use(function(req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', false);
+    // Pass to next layer of middleware
+    next();
+});
 
 // Routes
 /*-------------------------------------------------------------------API-------------------------------------------------------------------*/
@@ -74,18 +87,4 @@ app.use('/llamadas/api/indicadores', require('./routes/api/indicadores'));
 // Starting
 app.listen(app.get('port'), () => {
     console.log('Server is in port', app.get('port'));
-});
-
-app.use(function(req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    // Pass to next layer of middleware
-    next();
 });
