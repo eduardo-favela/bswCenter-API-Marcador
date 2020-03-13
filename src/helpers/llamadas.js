624e68ba364904ab641a3f26c27fc31f0f81fdcc
guerrerosantonio <guerrerosantonio@github.com>
2020-03-13 12:19:26 -0600

    Update llamadas.js

--- src/helpers/llamadas.js
+++ src/helpers/llamadas.js
@@ -20,7 +20,7 @@ module.exports.trasnferirLlamada = async (datos) => {
             return error.message;
         } else {
             fs.writeFile(archivo,
-                    `Channel:SIP/${extensionEscucha}
+                `Channel:SIP/${extensionEscucha}
                     Callerid:${extensionAgente}
                     WaitTime:30
                     Maxretries:1
@@ -36,7 +36,7 @@ module.exports.trasnferirLlamada = async (datos) => {
                         if (err) throw err;
                         return "Archivo escrito con exito";
                     });
-            });
+                });
         }
     });
 
@@ -48,43 +48,58 @@ module.exports.realizarLlamada = async (datos, res) => {
 
     //return "OK"
     let account = datos.campana + "." + datos.idCliente + "." + datos.extension;
-    let archivo = "/var/www/html/GenerarLlamada/llamadaOutbound" +account;
+    let archivo = "/var/www/html/GenerarLlamada/llamadaOutbound" + account;
     let infoArchivo = ``;
-    if(datos.campanaMod == "PREDICTIVA"){
-        infoArchivo = `${datos.channel}
-        Callerid:${datos.numeroFinal}
-        WaitTime:30
-        Maxretries:1
-        RetryTime:100
-        Extension:${datos.extension}
-        Account:${account}
-        Priority:1
-        Context:from-internal`;
-    }else{
-        infoArchivo = `Channel:SIP/${datos.extension}
-        Callerid:${datos.numero}
-        WaitTime:30
-        Maxretries:0
-        RetryTime:0
-        Extension:${datos.numeroFinal}
-        Account:${account}
-        Priority:1
-        Context:from-internal`;
+    if (datos.campanaMod == "PREDICTIVA") {
+        infoArchivo =
+            `${datos.channel}
+Callerid:${datos.numeroFinal}
+WaitTime:30
+Maxretries:1
+RetryTime:100
+Extension:${datos.extension}
+Account:${account}
+Priority:1
+Context:from-internal`;
+    } else {
+        infoArchivo =
+            `Channel:SIP/${datos.extension}
+Callerid:${datos.numero}
+WaitTime:30
+Maxretries:0
+RetryTime:0
+Extension:${datos.numeroFinal}
+Account:${account}
+Priority:1
+Context:from-internal`;
     }
     fs.open = util.promisify(fs.open)
     //fs.copyFile = util.promisify(fs.copyFile)
     //fs.writeFile = util.promisify(fs.writeFile)
     let leerFile = await fs.open(archivo, "w+");
-    if(leerFile.error) {
+    if (leerFile.error) {
         res.json("OK_NO") //leerFile.error.message;
-    }else {
+    } else {
         await fs.writeFile(archivo, infoArchivo,
-        async (err) => {
-            if (err) res.json("OK_NO");
-            await fs.copyFile(archivo, `/var/spool/asterisk/outgoing/llamadaOutbound`+account, (err) => {
-                if (err) res.json("OK_NO");
-                res.json("OK")
+            async (err) => {
+                if (err) {
+                    res.json("OK_NO");
+                } else {
+                    await fs.chmod(archivo, 0777, async (error) => {
+                        if (error) {
+                            res.json("OK_NO")
+                        } else {
+                            await fs.copyFile(archivo, `/var/spool/asterisk/outgoing/llamadaOutbound` + account, (err) => {
+                                if (err) res.json("OK_NO");
+                                res.json("OK")
+                            });
+                        }
+
+                    });
+
+
+                }
+
             });
-        });
     }
 }
